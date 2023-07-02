66512fb368317a297a92230c9062f545deac7ad2
Ivan Lukiev <113470410+F-mean-Fork@users.noreply.github.com>
2023-07-02 23:39:59 +0300

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,557 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('1.txt', 'r') as f1:
+    data = []
+    for line in f1.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x1 = data[:, 2]
+y1 = data[:, 0]
+
+with open('2.txt', 'r') as f2:
+    data = []
+    for line in f2.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x2 = data[:, 2]
+y2 = data[:, 0]
+
+with open('3.txt', 'r') as f3:
+    data = []
+    for line in f3.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x3 = data[:, 2]
+y3 = data[:, 0]
+
+with open('4.txt', 'r') as f4:
+    data = []
+    for line in f4.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x4 = data[:, 2]
+y4 = data[:, 0]
+
+with open('5.txt', 'r') as f5:
+    data = []
+    for line in f5.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x5 = data[:, 2]
+y5 = data[:, 0]
+
+with open('6.txt', 'r') as f6:
+    data = []
+    for line in f6.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x6 = data[:, 2]
+y6 = data[:, 0]
+
+with open('7.txt', 'r') as f7:
+    data = []
+    for line in f7.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x7 = data[:, 2]
+y7 = data[:, 0]
+
+with open('8.txt', 'r') as f8:
+    data = []
+    for line in f8.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x8 = data[:, 2]
+y8 = data[:, 0]
+
+with open('9.txt', 'r') as f9:
+    data = []
+    for line in f9.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x9 = data[:, 2]
+y9 = data[:, 0]
+
+with open('10.txt', 'r') as f10:
+    data = []
+    for line in f10.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x10 = data[:, 2]
+y10 = data[:, 0]
+
+with open('11.txt', 'r') as f11:
+    data = []
+    for line in f11.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x11 = data[:, 2]
+y11 = data[:, 0]
+
+with open('12.txt', 'r') as f12:
+    data = []
+    for line in f12.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x12 = data[:, 2]
+y12 = data[:, 0]
+
+with open('13.txt', 'r') as f13:
+    data = []
+    for line in f13.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x13 = data[:, 2]
+y13 = data[:, 0]
+
+with open('14.txt', 'r') as f14:
+    data = []
+    for line in f14.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x14 = data[:, 2]
+y14 = data[:, 0]
+
+with open('15.txt', 'r') as f15:
+    data = []
+    for line in f15.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x15 = data[:, 2]
+y15 = data[:, 0]
+
+with open('16.txt', 'r') as f16:
+    data = []
+    for line in f16.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x16 = data[:, 2]
+y16 = data[:, 0]
+
+with open('17.txt', 'r') as f17:
+    data = []
+    for line in f17.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x17 = data[:, 2]
+y17 = data[:, 0]
+
+with open('18.txt', 'r') as f18:
+    data = []
+    for line in f18.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x18 = data[:, 2]
+y18 = data[:, 0]
+
+with open('19.txt', 'r') as f19:
+    data = []
+    for line in f19.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x19 = data[:, 2]
+y19 = data[:, 0]
+
+with open('20.txt', 'r') as f20:
+    data = []
+    for line in f20.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x20 = data[:, 2]
+y20 = data[:, 0]
+
+with open('21.txt', 'r') as f21:
+    data = []
+    for line in f21.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x21 = data[:, 2]
+y21 = data[:, 0]
+
+with open('22.txt', 'r') as f22:
+    data = []
+    for line in f22.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x22 = data[:, 2]
+y22 = data[:, 0]
+
+with open('23.txt', 'r') as f23:
+    data = []
+    for line in f23.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x23 = data[:, 2]
+y23 = data[:, 0]
+
+with open('24.txt', 'r') as f24:
+    data = []
+    for line in f24.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x24 = data[:, 2]
+y24 = data[:, 0]
+
+with open('25.txt', 'r') as f25:
+    data = []
+    for line in f25.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x25 = data[:, 2]
+y25 = data[:, 0]
+
+with open('26.txt', 'r') as f26:
+    data = []
+    for line in f26.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x26 = data[:, 2]
+y26 = data[:, 0]
+
+with open('27.txt', 'r') as f27:
+    data = []
+    for line in f27.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x27 = data[:, 2]
+y27 = data[:, 0]
+
+with open('28.txt', 'r') as f28:
+    data = []
+    for line in f28.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x28 = data[:, 2]
+y28 = data[:, 0]
+
+with open('29.txt', 'r') as f29:
+    data = []
+    for line in f29.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x29 = data[:, 2]
+y29 = data[:, 0]
+
+with open('30.txt', 'r') as f30:
+    data = []
+    for line in f30.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x30 = data[:, 2]
+y30 = data[:, 0]
+
+with open('31.txt', 'r') as f31:
+    data = []
+    for line in f31.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x31 = data[:, 2]
+y31 = data[:, 0]
+
+with open('32.txt', 'r') as f32:
+    data = []
+    for line in f32.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x32 = data[:, 2]
+y32 = data[:, 0]
+
+with open('33.txt', 'r') as f33:
+    data = []
+    for line in f33.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x33 = data[:, 2]
+y33 = data[:, 0]
+
+with open('34.txt', 'r') as f34:
+    data = []
+    for line in f34.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x34 = data[:, 2]
+y34 = data[:, 0]
+
+with open('35.txt', 'r') as f35:
+    data = []
+    for line in f35.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x35 = data[:, 2]
+y35 = data[:, 0]
+
+with open('36.txt', 'r') as f36:
+    data = []
+    for line in f36.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x36 = data[:, 2]
+y36 = data[:, 0]
+
+with open('37.txt', 'r') as f37:
+    data = []
+    for line in f37.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x37 = data[:, 2]
+y37 = data[:, 0]
+
+with open('38.txt', 'r') as f38:
+    data = []
+    for line in f38.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x38 = data[:, 2]
+y38 = data[:, 0]
+
+with open('39.txt', 'r') as f39:
+    data = []
+    for line in f39.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x39 = data[:, 2]
+y39 = data[:, 0]
+
+with open('40.txt', 'r') as f40:
+    data = []
+    for line in f40.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x40 = data[:, 2]
+y40 = data[:, 0]
+
+with open('41.txt', 'r') as f41:
+    data = []
+    for line in f41.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x41 = data[:, 2]
+y41 = data[:, 0]
+
+with open('42.txt', 'r') as f42:
+    data = []
+    for line in f42.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x42 = data[:, 2]
+y42 = data[:, 0]
+
+with open('43.txt', 'r') as f43:
+    data = []
+    for line in f43.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x43 = data[:, 2]
+y43 = data[:, 0]
+
+with open('44.txt', 'r') as f44:
+    data = []
+    for line in f44.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x44 = data[:, 2]
+y44 = data[:, 0]
+
+with open('45.txt', 'r') as f45:
+    data = []
+    for line in f45.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x45 = data[:, 2]
+y45 = data[:, 0]
+
+with open('46.txt', 'r') as f46:
+    data = []
+    for line in f46.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x46 = data[:, 2]
+y46 = data[:, 0]
+
+with open('47.txt', 'r') as f47:
+    data = []
+    for line in f47.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x47 = data[:, 2]
+y47 = data[:, 0]
+
+with open('48.txt', 'r') as f48:
+    data = []
+    for line in f48.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x48 = data[:, 2]
+y48 = data[:, 0]
+
+with open('49.txt', 'r') as f49:
+    data = []
+    for line in f49.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x49 = data[:, 2]
+y49 = data[:, 0]
+
+with open('50.txt', 'r') as f50:
+    data = []
+    for line in f50.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x50 = data[:, 2]
+y50 = data[:, 0]
+
+with open('51.txt', 'r') as f51:
+    data = []
+    for line in f51.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x51 = data[:, 2]
+y51 = data[:, 0]
+
+with open('52.txt', 'r') as f52:
+    data = []
+    for line in f52.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x52 = data[:, 2]
+y52 = data[:, 0]
+
+with open('53.txt', 'r') as f53:
+    data = []
+    for line in f53.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x53 = data[:, 2]
+y53 = data[:, 0]
+
+with open('54.txt', 'r') as f54:
+    data = []
+    for line in f54.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x54 = data[:, 2]
+y54 = data[:, 0]
+
+with open('55.txt', 'r') as f55:
+    data = []
+    for line in f55.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x55 = data[:, 2]
+y55 = data[:, 0]
+
+with open('56.txt', 'r') as f56:
+    data = []
+    for line in f56.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x56 = data[:, 2]
+y56 = data[:, 0]
+
+with open('57.txt', 'r') as f57:
+    data = []
+    for line in f57.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x57 = data[:, 2]
+y57 = data[:, 0]
+
+with open('58.txt', 'r') as f58:
+    data = []
+    for line in f58.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x58 = data[:, 2]
+y58 = data[:, 0]
+
+with open('59.txt', 'r') as f59:
+    data = []
+    for line in f59.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x59 = data[:, 2]
+y59 = data[:, 0]
+
+with open('60.txt', 'r') as f60:
+    data = []
+    for line in f60.readlines():
+        data.append(list(map(float, line.split())))
+data = np.array(data)
+x60 = data[:, 2]
+y60 = data[:, 0]
+
+
+
+
+
+
+
+
+
+plt.plot(x1, y1, color = 'black', label = 'sigma = 0.01, N1 = 1, N2 = 59', linewidth= 1)
+plt.plot(x2, y2, color = 'red', label = 'sigma = 0.010471204188481676, N1 = 2, N2 = 58', linewidth= 1)
+plt.plot(x3, y3, color = 'gold', label = 'sigma = 0.010638297872340425, N1 = 3, N2 = 57', linewidth= 1)
+plt.plot(x4, y4, color = 'blue', label = 'sigma = 0.010810810810810811, N1 = 4, N2 = 56', linewidth= 1)
+plt.plot(x5, y5, color = 'green', label = 'sigma = 0.01098901098901099, N1 = 5, N2 = 55', linewidth= 1)
+plt.plot(x6, y6, color = 'violet', label = 'sigma = 0.0111731843575419, N1 = 6, N2 = 54', linewidth= 1)
+plt.plot(x7, y7, color = 'magenta', label = 'sigma = 0.011363636363636364, N1 = 7, N2 = 53', linewidth= 1)
+plt.plot(x8, y8, color = 'peru', label = 'sigma = 0.011560693641618497, N1 = 8, N2 = 52', linewidth= 1)
+plt.plot(x9, y9, color = 'darkseagreen', label = 'sigma = 0.011764705882352941, N1 = 9, N2 = 51', linewidth= 1)
+plt.plot(x10, y10, color = 'steelblue', label = 'sigma = 0.011976047904191617, N1 = 10, N2 = 50', linewidth= 1)
+plt.plot(x11, y11, color = 'pink', label = 'sigma = 0.012195121951219513, N1 = 11, N2 = 49', linewidth= 1)
+plt.plot(x12, y12, color = 'crimson', label = 'sigma = 0.012422360248447204, N1 = 12, N2 = 48', linewidth= 1)
+plt.plot(x13, y13, color = 'indigo', label = 'sigma = 0.012658227848101266, N1 = 13, N2 = 47', linewidth= 1)
+plt.plot(x14, y14, color = 'fuchsia', label = 'sigma = 0.012903225806451613, N1 = 14, N2 = 46', linewidth= 1)
+plt.plot(x15, y15, color = 'skyblue', label = 'sigma = 0.013157894736842105, N1 = 15, N2 = 45', linewidth= 1)
+plt.plot(x16, y16, color = 'olive', label = 'sigma = 0.013422818791946308, N1 = 16, N2 = 44', linewidth= 1)
+plt.plot(x17, y17, color = 'seagreen', label = 'sigma = 0.0136986301369863, N1 = 17, N2 = 43', linewidth= 1)
+plt.plot(x18, y18, color = 'teal', label = 'sigma = 0.013986013986013986, N1 = 18, N2 = 42', linewidth= 1)
+plt.plot(x19, y19, color = 'darkred', label = 'sigma = 0.014285714285714285, N1 = 19, N2 = 41', linewidth= 1)
+plt.plot(x20, y20, color = 'darkseagreen', label = 'sigma = 0.014598540145985401, N1 = 20, N2 = 40', linewidth= 1)
+plt.plot(x21, y21, color = 'navy', label = 'sigma = 0.014925373134328358, N1 = 21, N2 = 39', linewidth= 1)
+plt.plot(x22, y22, color = 'salmon', label = 'sigma = 0.015267175572519083, N1 = 22, N2 = 38', linewidth= 1)
+plt.plot(x23, y23, color = 'black', linestyle = '--', label = 'sigma = 0.015625, N1 = 23, N2 = 37', linewidth= 1)
+plt.plot(x24, y24, color = 'red', linestyle = '--', label = 'sigma = 0.016, N1 = 24, N2 = 36', linewidth= 1)
+plt.plot(x25, y25, color = 'gold', linestyle = '--', label = 'sigma = 0.01639344262295082, N1 = 25, N2 = 35', linewidth= 1)
+plt.plot(x26, y26, color = 'blue', linestyle = '--', label = 'sigma = 0.01680672268907563, N1 = 26, N2 = 34', linewidth= 1)
+plt.plot(x27, y27, color = 'green', linestyle = '--', label = 'sigma = 0.017241379310344827, N1 = 27, N2 = 33', linewidth= 1)
+plt.plot(x28, y28, color = 'violet', linestyle = '--', label = 'sigma = 0.017699115044247787, N1 = 28, N2 = 32', linewidth= 1)
+plt.plot(x29, y29, color = 'magenta', linestyle = '--', label = 'sigma = 0.01818181818181818, N1 = 29, N2 = 31', linewidth= 1)
+plt.plot(x30, y30, color = 'peru', linestyle = '--', label = 'sigma = 0.018691588785046728, N1 = 30, N2 = 30', linewidth= 1)
+plt.plot(x31, y31, color = 'darkseagreen', linestyle = '--', label = 'sigma = 0.019230769230769232, N1 = 31, N2 = 29', linewidth= 1)
+plt.plot(x32, y32, color = 'steelblue', linestyle = '--', label = 'sigma = 0.019801980198019802, N1 = 32, N2 = 28', linewidth= 1)
+plt.plot(x33, y33, color = 'pink', linestyle = '--', label = 'sigma = 0.02040816326530612, N1 = 33, N2 = 27', linewidth= 1)
+plt.plot(x34, y34, color = 'crimson', linestyle = '--', label = 'sigma = 0.021052631578947368, N1 = 34, N2 = 26', linewidth= 1)
+plt.plot(x35, y35, color = 'indigo', linestyle = '--', label = 'sigma = 0.021739130434782608, N1 = 35, N2 = 25', linewidth= 1)
+plt.plot(x36, y36, color = 'fuchsia', linestyle = '--', label = 'sigma = 0.02247191011235955, N1 = 36, N2 = 24', linewidth= 1)
+plt.plot(x37, y37, color = 'skyblue', linestyle = '--', label = 'sigma = 0.023255813953488372, N1 = 37, N2 = 23', linewidth= 1)
+plt.plot(x38, y38, color = 'olive', linestyle = '--', label = 'sigma = 0.024096385542168676, N1 = 38, N2 = 22', linewidth= 1)
+plt.plot(x39, y39, color = 'seagreen', linestyle = '--', label = 'sigma = 0.025, N1 = 39, N2 = 21', linewidth= 1)
+plt.plot(x40, y40, color = 'teal', linestyle = '--', label = 'sigma = 0.025974025974025976, N1 = 40, N2 = 20', linewidth= 1)
+plt.plot(x41, y41, color = 'black', linestyle = ':', label = 'sigma = 0.02702702702702703, N1 = 41, N2 = 19', linewidth= 1)
+plt.plot(x42, y42, color = 'red', linestyle = ':', label = 'sigma = 0.028169014084507043, N1 = 42, N2 = 18', linewidth= 1)
+plt.plot(x43, y43, color = 'gold', linestyle = ':', label = 'sigma = 0.029411764705882353, N1 = 43, N2 = 17', linewidth= 1)
+plt.plot(x44, y44, color = 'blue', linestyle = ':', label = 'sigma = 0.03076923076923077, N1 = 44, N2 = 16', linewidth= 1)
+plt.plot(x45, y45, color = 'green', linestyle = ':', label = 'sigma = 0.03225806451612903, N1 = 45, N2 = 15', linewidth= 1)
+plt.plot(x46, y46, color = 'violet', linestyle = ':', label = 'sigma = 0.03389830508474576, N1 = 46, N2 = 14', linewidth= 1)
+plt.plot(x47, y47, color = 'magenta', linestyle = ':', label = 'sigma = 0.03571428571428571, N1 = 47, N2 = 13', linewidth= 1)
+plt.plot(x48, y48, color = 'peru', linestyle = ':', label = 'sigma = 0.03773584905660377, N1 = 48, N2 = 12', linewidth= 1)
+plt.plot(x49, y49, color = 'darkseagreen', linestyle = ':', label = 'sigma = 0.04, N1 = 49, N2 = 11', linewidth= 1)
+plt.plot(x50, y50, color = 'steelblue', linestyle = ':', label = 'sigma = 0.0425531914893617, N1 = 50, N2 = 10', linewidth= 1)
+plt.plot(x51, y51, color = 'pink', linestyle = ':', label = 'sigma = 0.045454545454545456, N1 = 51, N2 = 9', linewidth= 1)
+plt.plot(x52, y52, color = 'crimson', linestyle = ':', label = 'sigma = 0.04878048780487805, N1 = 52, N2 = 8', linewidth= 1)
+plt.plot(x53, y53, color = 'indigo', linestyle = ':', label = 'sigma = 0.05263157894736842, N1 = 53, N2 = 7', linewidth= 1)
+plt.plot(x54, y54, color = 'fuchsia', linestyle = ':', label = 'sigma = 0.05714285714285714, N1 = 54, N2 = 6', linewidth= 1)
+plt.plot(x55, y55, color = 'skyblue', linestyle = ':', label = 'sigma = 0.0625, N1 = 55, N2 = 5', linewidth= 1)
+plt.plot(x56, y56, color = 'olive', linestyle = ':', label = 'sigma = 0.06896551724137931, N1 = 56, N2 = 4', linewidth= 1)
+plt.plot(x57, y57, color = 'seagreen', linestyle = ':', label = 'sigma = 0.07692307692307693, N1 = 57, N2 = 3', linewidth= 1)
+plt.plot(x58, y58, color = 'teal', linestyle = ':', label = 'sigma = 0.08695652173913043, N1 = 58, N2 = 2', linewidth= 1)
+plt.plot(x59, y59, color = 'darkred', linestyle = ':', label = 'sigma = 0.1, N1 = 59, N2 = 1', linewidth= 1)
+plt.plot(x60, y60, color = 'darkseagreen', linestyle = ':', label = 'sigma = 0.1, N1 = 60, N2 = 0', linewidth= 1)
+
+plt.ylabel('Energy', size=18)
+
+plt.legend(bbox_to_anchor=( 1 , 1.0 ), loc='upper left', borderaxespad= 0.5, fontsize = 6)
+plt.show()
+
